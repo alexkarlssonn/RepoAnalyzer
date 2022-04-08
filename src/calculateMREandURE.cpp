143e6e3fa612bdbf66a3d4631aad16f61ab57470
Alexander Karlsson <alka1905@student.miun.se>
2022-04-08 21:05:25 +0200

    Updated the algorithm that calculates MRE

--- src/calculateMREandURE.cpp
+++ src/calculateMREandURE.cpp
@@ -28,12 +28,29 @@ int calculateMREandURE(std::vector<AuthorData>& all_authors, int* MRE)
 		return (ad1.lastCommit - ad1.firstCommit) < (ad2.lastCommit - ad2.firstCommit);
 	});
 
+
 	// Calculate the MRE value
-	int mean_index = (all_authors.size() / 2);
-	if (mean_index < 0 || mean_index >= all_authors.size()) {
-		return -1;
+	if (all_authors.size() % 2 == 0) 
+	{
+		// Even number of authors, calculate the average of the two authors in the middle
+		int mean_index = (all_authors.size() / 2);
+		if (mean_index < 0 || mean_index + 1 >= all_authors.size()) {
+			return -1;
+		}
+		int MRE_1 = (all_authors[mean_index].lastCommit - all_authors[mean_index].firstCommit);
+		int MRE_2 = (all_authors[mean_index + 1].lastCommit - all_authors[mean_index + 1].firstCommit);
+		*MRE = (int)((MRE_1 + MRE_2) / 2);
 	}
-	*MRE = (all_authors[mean_index].lastCommit - all_authors[mean_index].firstCommit);
+	else
+	{
+		// Odd number of authors, the mean is in the middle
+		int mean_index = (all_authors.size() / 2);
+		if (mean_index < 0 || mean_index >= all_authors.size()) {
+			return -1;
+		}
+		*MRE = (all_authors[mean_index].lastCommit - all_authors[mean_index].firstCommit);
+	}
+
 
 	// Calculate the URE value for each author
 	for (int i = 0; i < all_authors.size(); i++) {
